3014b2661fe215f5cc819792143ebf9e99a15fe5
Kamogelo-MM <kammolawa@gmail.com>
2024-02-21 14:16:24 +0200

    update
    navbar
    scroll-effect

--- src/App.js
+++ src/App.js
@@ -10,23 +10,48 @@ const options = {
 async function displayFeed() {
   try {
     const response = await axios.request(options);
-    const TvShowsTitle = response.data.movies;
-    const TvShowsTitleDisplay = document.getElementById("tvShow-title");
-    const tvShowsDisplay = document.getElementById("tv-shows-display");
+    const shows1 = response.data[0].movies;
+    const show2 = response.data[1].movies;
+    const show3 = response.data[2].movies;
+    const show4 = response.data[3].movies;
+    const show5 = response.data[4].movies;
+    const displayShow5 = document.getElementById("adrenaline");
+    const displayShow4 = document.getElementById("family-time");
+    const displayShow3 = document.getElementById("best-animation");
+    const displayShow2 = document.getElementById("new-movies");
+    const displayShow1 = document.getElementById("trending-show");
 
-    TvShows.forEach((show) => {
-      const showElement = document.createElement("div");
-      showElement.classList.add("show");
-      showElement.innerHTML = `<img src='${show.backdrop_path}' alt='show-title'/>`;
-      tvShowsDisplay.appendChild(showElement);
+    show5.forEach((movie) => {
+      const movieElement = document.createElement("div");
+      movieElement.innerHTML = `<img src=${movie.backdrop_path}>`;
+      displayShow5.appendChild(movieElement);
     });
 
-    TvShowsTitle.forEach((title) => {
-      const showElement = document.createElement("div");
-      showElement.classList.add("title");
-      showElement.innerHTML = `<h2>${title}</h2>`;
-      TvShowsTitleDisplay.appendChild(showElement);
+    show4.forEach((movie) => {
+      const movieElement = document.createElement("div");
+      movieElement.innerHTML = `<img src=${movie.backdrop_path} >`;
+      displayShow4.appendChild(movieElement);
     });
+
+    show3.forEach((movie) => {
+      const movieElement = document.createElement("div");
+      movieElement.innerHTML = `<img src=${movie.backdrop_path} >`;
+      displayShow3.appendChild(movieElement);
+    });
+
+    show2.forEach((movie) => {
+      const movieElement = document.createElement("div");
+      movieElement.innerHTML = `<img src=${movie.backdrop_path}>`;
+      displayShow2.appendChild(movieElement);
+    });
+
+    shows1.forEach((movie) => {
+      const movieElement = document.createElement("div");
+      movieElement.innerHTML = `<img src=${movie.backdrop_path}>'`;
+
+      displayShow1.appendChild(movieElement);
+    });
+
     console.log(response.data);
   } catch (error) {
     console.error(error);
